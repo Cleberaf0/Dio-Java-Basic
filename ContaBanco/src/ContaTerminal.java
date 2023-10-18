4045a73cabded7d984f24a6d41467c38d48989aa
Cleberaf0 <cleberjrlopes@hotmail.com>
2023-10-17 22:11:05 -0300

    Projetos e desafio

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,40 @@
+/**
+* <h1>ContaTerminal</h1>
+* A ContaTerminal realiza a criação da conta do usuário
+* <p>
+* <b>Note:</b> Leia atentamente a documentação desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author  Cleber Augusto
+* @version 1.0
+* @since   17/10/2023
+*/
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência");
+        String agencia = scanner.next();
+        
+        System.out.println("Por favor, digite o número da Conta");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Quanto deseja depositar ?");
+        double saldo = scanner.nextDouble();
+        
+
+                
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + 
+        ", conta " + numero +  ", e seu saldo de $" + saldo + " já está disponível para saque.");
+
+                
+    }
+}
---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"ContaTerminal","l":"ContaTerminal()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"App","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"ContaTerminal","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
